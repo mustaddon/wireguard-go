1336e82c7f2bc20ff9374017cc413567ad0ae385
mustaddon <mustaddon@gmail.com>
2026-05-04 23:09:25 +0200

    tst

--- device/hidden.go
+++ device/hidden.go
@@ -17,7 +17,7 @@ func AddHiddenHeader(packet []byte, msgType uint32) []byte {
 		binary.LittleEndian.PutUint32(hidden, uint32(time.Now().UnixNano()>>hlen))
 	}
 	hidden[0] = rnd
-	copy(hidden[hlen:], packet[0:])
+	copy(hidden[hlen:], packet)
 	return hidden
 }
 
@@ -30,6 +30,6 @@ func MsgHiddenType(val uint32) uint32 {
 }
 
 func HiddenType(val uint32) uint32 {
-	result := uint32(time.Now().UnixNano())
-	return (result &^ 0xFF) | uint32(int(result&0xFF)+int(val)-int(MsgHiddenType(result)))
+	noise := uint32(time.Now().UnixNano())
+	return (noise &^ 0xFF) | uint32(int(noise&0xFF)+int(val)-int(MsgHiddenType(noise)))
 }
