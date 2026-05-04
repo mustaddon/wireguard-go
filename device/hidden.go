016bd3bdb8df3ad84532f04ee804a4cde77601b3
mustaddon <mustaddon@gmail.com>
2026-05-04 22:51:40 +0200

    tst

--- device/hidden.go
+++ device/hidden.go
@@ -1,15 +1,23 @@
 package device
 
 import (
+	"encoding/binary"
+	"math/rand/v2"
 	"time"
-	"unsafe"
 )
 
 func AddHiddenHeader(packet []byte, msgType uint32) []byte {
-	hidden := make([]byte, len(packet)+2)
-	copy(hidden[2:], packet[0:])
-	hidden[0] = 1
-	hidden[1] = byte(msgType)
+	rnd := byte(rand.UintN(256))
+	if rnd < 16 {
+		rnd |= 0x10
+	}
+	hlen := HiddenHeaderLen(rnd)
+	hidden := make([]byte, len(packet)+hlen)
+	if hlen > 1 {
+		binary.LittleEndian.PutUint32(hidden, uint32(time.Now().UnixNano()>>hlen))
+	}
+	hidden[0] = rnd
+	copy(hidden[hlen:], packet[0:])
 	return hidden
 }
 
@@ -23,7 +31,5 @@ func MsgHiddenType(val uint32) uint32 {
 
 func HiddenType(val uint32) uint32 {
 	result := uint32(time.Now().UnixNano())
-	byteSlice := (*[4]byte)(unsafe.Pointer(&result))[:]
-	byteSlice[0] = byte(int(byteSlice[0]) + (int(val) - int(MsgHiddenType(result))))
-	return result
+	return (result &^ 0xFF) | uint32(int(result&0xFF)+int(val)-int(MsgHiddenType(result)))
 }
